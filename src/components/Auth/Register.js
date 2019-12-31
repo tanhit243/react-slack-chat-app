1b0dd4b9cc6300258f205a60608a979f7bc695e6
Tuan Anh <tanhit243@gmail.com>
2019-12-31 19:22:52 +0700

    Validation

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Form, Segment, Button, Header, Message, Icon, GridColumn, FormInput } from "semantic-ui-react";
 import { Link } from 'react-router-dom';
+import firebase from '../../firebase';
 
 class Register extends React.Component {
     constructor(props) {
@@ -9,15 +10,77 @@ class Register extends React.Component {
             username: '',
             email: '',
             password: '',
-            passwordConfirmation: ''
+            passwordConfirmation: '',
+            errors: []
         };
 
         // This binding is necessary to make `this` work in the callback
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.isFormEmpty = this.isFormEmpty.bind(this);
+        this.isFormVaild = this.isFormVaild.bind(this);
+        this.isPasswordVaild = this.isPasswordVaild.bind(this);
+        this.displayError = this.displayError.bind(this);
+    }
+
+    isFormVaild() {
+        let error;
+        let errors = [];
+
+        if(this.isFormEmpty(this.state)) {
+            error = { message: 'Fill in all fields' };
+            this.setState({errors: errors.concat(error)});
+            return false;
+        } else if(!this.isPasswordVaild(this.state)) {
+            error = {message: 'Password is invaild'};
+            this.setState({errors: errors.concat(error)});
+            return false;
+        }
+        return true;
+    }
+
+    isFormEmpty({username, email, password, passwordConfirmation}) {
+        return (
+            !username.length ||
+            !email.length ||
+            !password.length ||
+            !passwordConfirmation.length
+        );
+    }
+
+    isPasswordVaild({password,passwordConfirmation}) {
+        if(password.length >= 6) {
+            if(password === passwordConfirmation) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    displayError(errors) {
+        console.log(errors.map((error, i) => <p key={i}>{error.message}</p>));
+        return errors.map((error, i) => <p key={i}>{error.message}</p>);
     }
 
     handleChange(event) {
         this.setState({ [event.target.name ]: event.target.value });
+    }
+
+    handleSubmit(event) {
+        if(this.isFormVaild()) {
+            event.preventDefault();
+            let email = event.target.email.value;
+            let password = event.target.password.value;
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(email,password)
+                .then(user => {
+                    console.log(user);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
     }
 
     render () {
@@ -28,15 +91,24 @@ class Register extends React.Component {
                         <Icon name='slack' />
                         Register for chatA
                     </Header>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Segment>
                             <FormInput icon='user'  iconPosition='left' placeholder='Username' type='text' name='username' onChange={this.handleChange} value={this.state.username} />
-                            <FormInput icon='mail'  iconPosition='left' placeholder='Email' type='email' onChange={this.handleChange} />
-                            <FormInput icon='write' iconPosition='left' placeholder='Password' type='password' onChange={this.handleChange} />
-                            <FormInput icon='repeat' iconPosition='left' placeholder='Password Confirmation' type='password' onChange={this.handleChange} />
+                            <FormInput icon='mail'  iconPosition='left' placeholder='Email' type='email' name='email' onChange={this.handleChange} value={this.state.email} />
+                            <FormInput icon='write' iconPosition='left' placeholder='Password' type='password' name='password' onChange={this.handleChange} vaule={this.state.password} />
+                            <FormInput icon='repeat' iconPosition='left' placeholder='Password Confirmation' type='password' name='passwordConfirmation' onChange={this.handleChange} value={this.state.passwordConfirmation} />
                             <Button fluid color='orange'>Submit</Button>
                         </Segment>
                     </Form>
+                    {
+                        this.state.errors.length > 0 &&
+                        (
+                            <Message error>
+                                <Message.Header>Error</Message.Header>
+                                {this.displayError(this.state.errors)}                  
+                            </Message>
+                        )
+                    }
                     <Message>
                         <p>New to chatA? <Link to='/login' >Login</Link></p>
                     </Message>
@@ -47,3 +119,4 @@ class Register extends React.Component {
 }
 
 export default Register;
+//https://reactjs.org/docs/introducing-jsx.html : parentheses,the curly braces
